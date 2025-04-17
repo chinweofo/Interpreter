0ae4b5336b7fd916341695970f7328e32c9348cf
Chinwe Ofonagoro <98469853+chinweofo@users.noreply.github.com>
2025-04-16 23:54:02 -0400

    Update Parser.java

--- src/Tran/Parser.java
+++ src/Tran/Parser.java
@@ -6,9 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-/*      Questions
-* 2. Fix ParseBoolExpTerm()
-* */
+
 
 public class Parser {
     private final TranNode top;
